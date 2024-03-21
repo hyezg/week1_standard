5cd14400658560d7a59df403b977153e3089749f
hyejeong <112006114+hyezg@users.noreply.github.com>
2024-03-21 18:34:21 +0900

    Add: xml파일

--- app/src/main/java/com/example/w1_s/Intro.kt
+++ app/src/main/java/com/example/w1_s/Intro.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class Intro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.intro)
+        setContentView(R.layout.activity_intro)
     }
 }
