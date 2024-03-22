a3da165ff59b2afcaa2bfc0217b20268729c49e5
hyejeong <112006114+hyezg@users.noreply.github.com>
2024-03-22 15:25:49 +0900

    Add: intro에서 onClick하는거 추가

--- app/src/main/java/com/example/w1_s/Intro.kt
+++ app/src/main/java/com/example/w1_s/Intro.kt
@@ -1,11 +1,20 @@
 package com.example.w1_s
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class Intro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        val signbtn = findViewById<ImageButton>(R.id.btnsign)
+        signbtn.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+        }
+
     }
 }
